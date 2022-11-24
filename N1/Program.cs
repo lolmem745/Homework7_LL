338b1d1c808f9e5468fdfad17bf6b6b6d59bd493
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-24 20:30:02 +0300

    First problem solved

---
+++ N1/Program.cs
@@ -0,0 +1,35 @@
+﻿void fillArray(double[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().NextDouble() * 100;
+            string result = arr[i, j].ToString("#.##");
+            arr[i, j] = Convert.ToDouble(result);
+        }
+
+    }
+}
+void printArray(double[,] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}");
+            if (j != arr.GetLength(1) - 1)
+                Console.Write(", ");
+        }
+        if (i == arr.GetLength(0) - 1) Console.Write("]");
+        Console.Write($"\n");
+
+    }
+
+}
+int m = 3, n = 5;
+double[,] arr = new double[m, n];
+fillArray(arr);
+printArray(arr);
